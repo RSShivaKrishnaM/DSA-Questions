0ca0ff9a309bef222bd58737e7b38ec1fef62544
rsshivakrishnam <rsshivakrishnam@localhost>
2022-02-08 22:40:22 +0530

    DSA project structure

---
+++ Src/com/dsaNAlgo/dsa/Arrays/twoPointers/RainTappingProblem.java
@@ -0,0 +1,16 @@
+package com.dsaNAlgo.dsa.Arrays.twoPointers;
+import java.lang.*;
+
+public class RainTappingProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void Solution() {
+		
+	}
+}
+
+
