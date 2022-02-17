6638ae1a83ce50413182a5b6e027e392b555f514
rsshivakrishnam <rsshivakrishnam@localhost>
2022-02-17 06:39:37 +0530

    283 move zeros

--- Src/arrays/slidingWindow/MoveZeros.java
+++ Src/arrays/slidingWindow/MoveZeros.java
@@ -32,4 +32,17 @@ public class MoveZeros {
 	            }
 	        }
 	    }
+	   
+	   public void moveZerosSolu2(int []nums) {
+		   int nonZero =0;
+	        
+	        for(int i=0; i<nums.length; i++){
+	            if(nums[i]!=0)
+	                nums[nonZero++]= nums[i];
+	        }
+	        
+	        for (int i= nonZero; i<nums.length;i++){
+	            nums[i]=0;
+	        }
+	   }
 }
