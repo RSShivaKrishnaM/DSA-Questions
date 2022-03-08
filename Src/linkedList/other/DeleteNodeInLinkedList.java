807cfbc84634c2e073857db54465e704b5dc81ee
rsshivakrishnam <rsshivakrishnam@localhost>
2022-03-08 22:03:17 +0530

    https://leetcode.com/problems/delete-node-in-a-linked-list/

---
+++ Src/linkedList/other/DeleteNodeInLinkedList.java
@@ -0,0 +1,11 @@
+package linkedList.other;
+
+public class DeleteNodeInLinkedList {
+	public void deleteNode(ListNode node) {
+		if (node == null || node.next == null) {
+			return;
+		}
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+}
