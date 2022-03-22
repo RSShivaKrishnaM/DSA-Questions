da0dba604b3f54e6b8a612a7c71858900942b359
rsshivakrishnam <rsshivakrishnam@localhost>
2022-03-22 15:37:15 +0530

    https://leetcode.com/problems/binary-tree-level-order-traversal/

--- Src/trees/traversals/BinaryTreeLevelOrderTraversal.java
+++ Src/trees/traversals/BinaryTreeLevelOrderTraversal.java
@@ -5,8 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import trees.traversals.BinaryTreePreOrderTraversal.TreeNode;
-
 public class BinaryTreeLevelOrderTraversal {
 	public class TreeNode {
 	      int val;
