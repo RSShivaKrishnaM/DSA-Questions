f2fffae7142b61b0807c9673ee1f3040637f9c3a
Shiva Meduri <rsshivakrishnam@gmail.com>
2023-11-28 16:07:44 +0530

    majorityElements 1 and 2

--- DSA_questions/Src/arrays/kadensAlgo/MajorityElementII.py
+++ DSA_questions/Src/arrays/kadensAlgo/MajorityElementII.py
@@ -14,7 +14,43 @@ class Solution:
             if(value>math.floor(n/3)):
                 majorEle.append(key)
         return majorEle
+    
+    def majorityElement_2(self, nums: List[int]) -> List[int]:
+        majorEle = []
+        cont_1 = 0
+        cont_2 = 0
+        ele1 = nums[cont_1]
+        ele2 = nums[cont_2]
+        
+        for num in nums:
+            if(cont_1==0 & num!=ele2):
+                cont_1 = 1
+                ele1 = num
+            elif(cont_2==0 & num!=ele1):
+                cont_2 = 1
+                ele2 = num
+            elif(ele1==num):
+                cont_1 += 1
+            elif(ele2==num):
+                cont_2 += 1
+            else:
+                cont_1 -= 1
+                cont_2 -= 1
+        cont_1=0
+        cont_2=0
+        for num in nums:
+            if num == ele1:
+                cont_1 += 1
+            if num == ele2:
+                cont_2 += 1
+                
+        if(cont_1 >= len(nums)/3 +1):
+            majorEle.append(ele1)
+        if(cont_2 >= len(nums)/3 +1):
+            majorEle.append(ele2)
+        
+        return majorEle
 
 if __name__ == "__main__":
     sol = Solution
-    print(sol.majorityElement_1(sol, [1,3,2,1]))
+    print(sol.majorityElement_1(sol, [1,3,2,1,2]))
--- DSA_questions/Src/arrays/kadensAlgo/majorElement.py
+++ DSA_questions/Src/arrays/kadensAlgo/majorElement.py
@@ -8,6 +8,7 @@ class Solution:
         count = 1
         for i in range(1, len(nums)):
             if(count == 0):
+                count = 1
                 ele = nums[i]
             if(nums[i] == ele):
                 count += 1
